63dafa96577c936bf676996cc138168192ec0511
Jerry Janvier <jerrythefilmmaker@gmail.com>
2024-01-30 20:06:19 -0500

    function lab

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(someValue) {
+        let blocks = 42 - someValue
+        return Math.abs(blocks)
+}
+
+function distanceFromHqInFeet(someValue) {
+    
+    return distanceFromHqInBlocks(someValue) * 264
+    
+}
+
+function distanceTravelledInFeet(start, dest) {
+    return 264 * (Math.abs(start - dest))
+}
+
+function calculatesFarePrice(start, dest) {
+    let total = 264 * (Math.abs(start - dest))
+    if (total < 400) {
+        return 0
+    } else if (total >= 400 && total <= 2000) {
+        return .02 * (total - 400)
+    } else if (total > 2000 && total <= 2500 ) {
+        return 25
+    } else {
+        return "cannot travel that far"
+    }
+}
